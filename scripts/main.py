f5119c7aff795255cdf9560fdc4273806ca52167
denilv <denilv@mail.ru>
2020-03-17 04:47:45 +0300

    [up]: image cropping is not out of Predictor; gpu option

--- scripts/main.py
+++ scripts/main.py
@@ -8,7 +8,7 @@ import cv2
 import torch
 
 from modules.comp_tools import preprocessing_fn
-from modules.utils import crop_img, find_face, open_img
+from modules.utils import crop_img, find_face, open_img, resize_shortest_edge
 
 
 def eprint(arg):
@@ -20,6 +20,7 @@ def default_argument_parser():
     parser.add_argument('model', help='Path to torchscript model')
     parser.add_argument('fld', help='Folder with images')
     parser.add_argument('--threshold', help='Decision threshold', type=float, default=0.5)
+    parser.add_argument('--use-gpu', action='store_true', help='Whether to use gpu or not')
     return parser
 
 
@@ -27,27 +28,22 @@ class Predictor():
 
     def __init__(
         self, 
-        model, 
+        model,
+        device,
         th=0.5, 
         shape=(120, 120),
         preprocessing_fn=preprocessing_fn,
     ):
         self.model = model
+        self.device = device
         self.th = th
         self.activation = torch.sigmoid
         self.shape = shape
 
     def predict(self, img):
-        bbox = find_face(img)
-        if bbox is None:
-            # cannot find face on image
-            return 0
-        top, bot, left, right = bbox
-        crop = img[top: bot, left: right] / 255.
-        # crop = crop_img(img) / 255.
-        crop = cv2.resize(crop, self.shape)
+        crop = cv2.resize(img, self.shape)
         crop = preprocessing_fn(crop)
-        tensor = torch.Tensor(crop).unsqueeze(0)
+        tensor = torch.Tensor(crop).unsqueeze(0).to(self.device)
         with torch.no_grad():
             logit = self.model(tensor)
         prob = self.activation(logit)
@@ -56,8 +52,9 @@ class Predictor():
 
 if __name__=='__main__':
     args = default_argument_parser().parse_args()
-    model = torch.jit.load(args.model)
-    predictor = Predictor(model, th=args.threshold)
+    device = 'cuda' if args.use_gpu else 'cpu'
+    model = torch.jit.load(args.model, map_location=device)
+    predictor = Predictor(model, device, th=args.threshold)
     prediction_time = 0
     start = time.time()
     fnames = os.listdir(args.fld)
@@ -66,8 +63,14 @@ if __name__=='__main__':
         for fname in fnames:
             fp = osp.join(args.fld, fname)
             img = open_img(fp)
+            bbox = find_face(img)
+            if bbox is None:
+                # cannot find face on image
+                continue
+            top, bot, left, right = bbox
+            crop = img[top: bot, left: right] / 255.
             prediction_start = time.time()
-            prediction = predictor.predict(img)
+            prediction = predictor.predict(crop)
             prediction_time += time.time() - prediction_start
             if prediction==1:
                 print(fp)
